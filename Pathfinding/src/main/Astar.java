aefeca0677e2e03cbaf5e42137a930fd2d8f7f52
Abhay Zala <zalaa@Spectre.localdomain>
2020-02-23 21:48:10 -0500

    Init the Astar method

--- Pathfinding/src/main/Astar.java
+++ Pathfinding/src/main/Astar.java
@@ -1,5 +1,20 @@
 package main;
 
+import java.awt.Point;
+
 public class Astar {
 
+	public static Node[][] maze;
+	
+	public static void main(String[] args) {
+		maze = new Node[Integer.parseInt(args[0])][Integer.parseInt(args[1])];
+		
+		
+		
+	}
+	
+	public static void AstarPath(Point start, Point end) {
+		
+	}
+	
 }
