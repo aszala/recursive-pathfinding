8f65118672e3286e9db23fb2aca87a1bef6d5a7d
Abhay Zala <zalaa@Spectre.localdomain>
2020-02-23 22:03:11 -0500

    Started Astar

--- Pathfinding/src/main/Astar.java
+++ Pathfinding/src/main/Astar.java
@@ -1,6 +1,8 @@
 package main;
 
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Astar {
 
@@ -9,12 +11,44 @@ public class Astar {
 	public static void main(String[] args) {
 		maze = new Node[Integer.parseInt(args[0])][Integer.parseInt(args[1])];
 		
+		Node start = new Node(new Point(0, 0));
+		Node end = new Node(new Point(maze.length-1, maze.length-1));
 		
-		
+		AstarPath(start, end);
 	}
 	
-	public static void AstarPath(Point start, Point end) {
+	public static List<Node> openSet;
+	
+	public static void AstarPath(Node start, Node end) {
+		openSet = new ArrayList<>();
 		
+		openSet.add(start);
+		
+		Node currentNode;
+		
+		start.g = 0;
+		start.f = H(start.position, end.position);
+		
+		while (openSet.size() > 0) {
+			currentNode = getBestNode();
+			
+			
+		}
+	}
+	
+	public static Node getBestNode() {
+		Node bestNode = openSet.get(0);
+		
+		for (Node node : openSet) {
+			if (node.f < bestNode.f) 
+				bestNode = node;
+		}
+		
+		return bestNode;
+	}
+	
+	public static float H(Point start, Point end) {
+		return (float) Math.sqrt(Math.pow(end.x-start.x, 2) + Math.pow(end.y-start.y, 2));
 	}
 	
 }
